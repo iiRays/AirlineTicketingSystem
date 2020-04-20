3822216f67519408883414afef3f2ff61b3357b5
Kelvin Ng <kelvinntk-sm17@student.tarc.edu.my>
2020-04-20 18:19:59 +0800

    Account updates

--- AirlineTicketingSystem/Account.vb
+++ AirlineTicketingSystem/Account.vb
@@ -1,9 +1,30 @@
 ﻿Public Class Account
     Private Sub Account_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TitleBar.Load(Me)
+        Dim user = If(App.User Is Nothing, App.User, App.User)
 
-        'Get user detail
-        txtName.Text &= App.User.Name
+        With user
+            txtName.Text = If(user IsNot Nothing AndAlso .Name IsNot Nothing, .Name, "")
+            txtEmail.Text = If(user IsNot Nothing AndAlso .Email IsNot Nothing, .Email, "")
+            txtCity.Text = If(user IsNot Nothing AndAlso .City IsNot Nothing, .City, "")
+            txtCountry.Text = If(user IsNot Nothing AndAlso .Country IsNot Nothing, .Country, "")
+            dtpDate.Value = If(user IsNot Nothing AndAlso .DateOfBirth IsNot Nothing, .DateOfBirth, DateAndTime.Now())
+            txtCreditCardNo.Text = If(user IsNot Nothing AndAlso .CreditCardNo IsNot Nothing, .CreditCardNo, "")
+        End With
+
+        If user IsNot Nothing Then
+            With user
+                If .Gender = "M" Then
+                    rbMale.Checked = True
+                ElseIf .Gender = "F" Then
+                    rbFemale.Checked = True
+                Else
+                    .Gender = "N"
+                End If
+            End With
+        Else
+            rbMale.Checked = True
+        End If
 
     End Sub
 
@@ -12,15 +33,47 @@
     End Sub
 
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
-        Dim user = DB.Get(Of User)(App.User.UserID)
-        user.name = txtName.Text
-        user.password = txtPassword.Text
-        user.email = txtEmail.Text
-        user.dateOfBirth = txtDate.Text
-        user.creditCardNo = txtCreditCardNo.Text
-        user.country = txtCountry.Text
-        user.city = txtCity.Text
-        'DB.Update(Of User)(App.User)
-        'App.User = updateUser
+        Dim errorStr As New System.Text.StringBuilder()
+
+        'Validate
+        errorStr.AppendLine(If(txtName.Text = "", "- Please enter a name", ""))
+        errorStr.AppendLine(If(txtEmail.Text = "", "- Please enter an email", ""))
+        errorStr.AppendLine(If(txtCity.Text = "", "- Please enter the city for your address", ""))
+        errorStr.AppendLine(If(txtCountry.Text = "", "- Please enter the country for your address", ""))
+        errorStr.AppendLine(If(Not Quick.CheckRegex("[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}", txtCreditCardNo.Text), "- Ensure that your credit card number is following this format: XXXX-XXXX-XXXX-XXXX.", ""))
+
+        'Must be 18 years old or above
+        Dim birthdate = New Date(dtpDate.Value.Year, dtpDate.Value.Month, dtpDate.Value.Day)
+        errorStr.AppendLine(If(DateAndTime.DateDiff(DateInterval.Year, birthdate, DateAndTime.Now()) < 18, "- You must be 18 years old or above", ""))
+
+
+        If errorStr.ToString.Trim.Length > 0 Then
+            'Has error
+            Quick.ShowError("Incorrect fields", errorStr.ToString().Trim)
+            Return
+        Else
+            If Not App.IsLoggedIn Then
+                App.User = New User()
+            End If
+            With App.User
+                .Name = txtName.Text
+                .Email = txtEmail.Text
+                .CreditCardNo = txtCreditCardNo.Text
+                .City = txtCity.Text
+                .Country = txtCountry.Text
+                .DateOfBirth = dtpDate.Value
+
+                If rbMale.Checked Then
+                    .Gender = "M"
+                ElseIf rbFemale.Checked Then
+                    .Gender = "F"
+                Else
+                    .Gender = "N"
+                End If
+            End With
+
+            DB.Update(App.User, App.User.UserID)
+            Quick.Navigate(Me, New HomeUser)
+        End If
     End Sub
 End Class
