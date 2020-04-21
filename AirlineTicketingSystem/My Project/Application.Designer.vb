6da2cd45e2bc5e23f3b1b14730f10864be60ea63
jacksonwjc99 <weijie11@hotmail.com.tw>
2020-04-21 09:51:14 +0800

    edit report 2.0

--- AirlineTicketingSystem/My Project/Application.Designer.vb
+++ AirlineTicketingSystem/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.AirlineTicketingSystem.HomeGuest
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.AirlineTicketingSystem.Splash
+        End Sub
     End Class
 End Namespace
