1a1f42cfcd2a831f520ad0f5b35ee2c842c86a9c
jacksonwjc99 <weijie11@hotmail.com.tw>
2020-04-22 01:02:19 +0800

    report edit

--- AirlineTicketingSystem/My Project/Application.Designer.vb
+++ AirlineTicketingSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.AirlineTicketingSystem.HomeGuest
+            Me.MainForm = Global.AirlineTicketingSystem.FlightScheduleReport
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
